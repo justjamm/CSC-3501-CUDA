12b1cc53655833f33ac6d8ef207885c3ef267d98
justjamm <benmarceaux@gmail.com>
2026-02-24 21:47:55 -0600

    added comments

--- Bennett-sort/cuda_sort.cu
+++ Bennett-sort/cuda_sort.cu
@@ -1,5 +1,3 @@
-
-
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -11,10 +9,12 @@
 
 int main(void) {
 
+	// Ask user for array length
 	int data_length = 0;
 	std::cout << "Enter length of array: ";
 	std::cin >> data_length;
 
+	// Create randomizer
 	std::random_device rand;
 	std::mt19937 gen(rand());
 	std::uniform_int_distribution<> dis(1,999);
@@ -22,21 +22,27 @@ int main(void) {
 	long sum;
 	for (int i=1;i<6;i++) {
 
+		// Create array & fill with random
 		std::vector<int> data(data_length);
 		std::generate(data.begin(), data.end(), [&]() { return dis(gen); });
 		thrust::device_vector<int> thrust_data = data;
 		
+		// Start timer
 		auto start = std::chrono::high_resolution_clock::now();
+
+		// Sort array
 		thrust::sort(thrust_data.begin(), thrust_data.end());
 		thrust::copy(thrust_data.begin(), thrust_data.end(), data.begin());
+		
+		// End timer and print
 		auto end = std::chrono::high_resolution_clock::now();
-
 		auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
 		std::cout << i <<  ". " << duration.count() << " ms\n";
 
 		sum += duration.count();
 	}
 
+	// Calculate average and print
 	sum /= 5;
 	std::cout << "Average: " << sum << " ms\n";
 	return 0;
--- Bennett-sort/standard_sort.cu
+++ Bennett-sort/standard_sort.cu
@@ -1,5 +1,3 @@
-
-
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -9,10 +7,12 @@
 
 int main(void) {
 
+	// Ask user for array length
 	int data_length = 0;
 	std::cout << "Enter length of array: ";
 	std::cin >> data_length;
 	
+	// Create randomizer
 	std::random_device rand;
 	std::mt19937 gen(rand());
 	std::uniform_int_distribution<> dis(1,999);
@@ -20,21 +20,26 @@ int main(void) {
 	long sum = 0;	
 	for (int i = 1; i<6; i++) {
 
+		// Create array and fill with random
 		std::vector<int> data(data_length);
 		std::generate(data.begin(), data.end(), [&]() { return dis(gen); });
 
+		// Start timer
 		auto start = std::chrono::high_resolution_clock::now();
-		std::sort(data.begin(), data.end());
-		auto end = std::chrono::high_resolution_clock::now();
 		
+		// Sort array with std::sort
+		std::sort(data.begin(), data.end());
+
+		// End timer and print
+		auto end = std::chrono::high_resolution_clock::now();
 		auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
 		std::cout << i << ". " << duration.count() << " ms\n";
 		
-
 		sum += duration.count();
 	}
 
+	// Calculate average and print
 	sum = sum / 5;
 	std::cout << "Average: " << sum << " ms\n";
 	return 0;
-}
+	}
